5c574ac3d28756066c1ac1a59fea7dd74e7e90d8
c-consalpa <c-constantine.alpashkin@edifecs.com>
2020-02-05 01:25:29 +0300

    golovach Courses; Maven web project added;

---
+++ GolovachEE_WEB/src/TestServlet.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+public class TestServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        OutputStream os = response.getOutputStream();
+        response.setContentType("text/html");
+        PrintWriter pw = new PrintWriter(os);
+        pw.println("<h1>qweqweqe</h1>");
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.println("<h1>qweqweqe</h1>");
+        pw.println("<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "  <meta charset=\"UTF-8\">\n" +
+                "  <title>Document</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "  \n" +
+                "</body>\n" +
+                "</html>");
+    }
+}
