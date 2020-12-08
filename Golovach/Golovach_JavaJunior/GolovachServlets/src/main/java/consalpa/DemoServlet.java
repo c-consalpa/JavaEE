deaad1505e4e9620ff9d909de5109f6836a895c9
Konstantin <konstantin.alpashkin@gmail.com>
2020-12-08 17:58:09 +0300

    no message

--- Golovach/Golovach_JavaJunior/GolovachServlets/src/main/java/consalpa/DemoServlet.java
+++ Golovach/Golovach_JavaJunior/GolovachServlets/src/main/java/consalpa/DemoServlet.java
@@ -1,6 +1,8 @@
 package consalpa;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,8 +17,12 @@ public class DemoServlet extends HttpServlet {
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println(request.getParameter("getData"));
+        Cookie mCookie = new Cookie("counter", "1");
+        response.addCookie(mCookie);
+        this.getServletContext().getRequestDispatcher("/CookieServlet").forward(request, response);
+
 
     }
 
