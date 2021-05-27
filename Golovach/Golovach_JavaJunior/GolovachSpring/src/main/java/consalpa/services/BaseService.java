f113d3280799f1a73911f27a2b541aa1ce76f57f
Konstantin Alpashkin <c-constantine.alpashkin@edifecs.com>
2021-05-27 21:01:32 +0300

    no message

--- Golovach/Golovach_JavaJunior/GolovachSpring/src/main/java/consalpa/services/BaseService.java
+++ Golovach/Golovach_JavaJunior/GolovachSpring/src/main/java/consalpa/services/BaseService.java
@@ -1,6 +1,10 @@
 package consalpa.services;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class BaseService implements mService {
+
     @Override
     public void serve() {
         System.out.println("BaseService: serve()");
