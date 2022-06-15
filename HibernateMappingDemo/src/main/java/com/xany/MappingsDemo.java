5fc2ab978c03e704dc657224105951e7ff0acba3
Konstantin_thndr <konstantin.alpashkin@gmail.com>
2022-06-15 17:48:11 +0300

    understanding on-2-many

--- HibernateMappingDemo/src/main/java/com/xany/MappingsDemo.java
+++ HibernateMappingDemo/src/main/java/com/xany/MappingsDemo.java
@@ -26,51 +26,55 @@ public class MappingsDemo {
             LecturerDetails lect1Details = new LecturerDetails("igor@mail.com");
             Lecturer l1 = new Lecturer("Igor");
             l1.setLecturerDetails(lect1Details);
-            session.save(l1);
+//            session.save(l1);
 
-            Lecturer tmp = session.get(Lecturer.class, 1l);
+//            Lecturer tmp = session.get(Lecturer.class, 1l);
             Review r = new Review();
             r.setReviewText("reviewA");
             Course c = new Course("jerking in a nutshell");
             c.addReview(r);
-
-
-
-//            LecturerDetails lect1Details2 = new LecturerDetails("maxx@mail.com");
-//            Lecturer l2 = new Lecturer("Max");
-//            l2.setLecturerDetails(lect1Details2);
-//            session.save(l2);
-
-
-
-
-
-
-
-            tmp.addCourse(c);
             session.save(c);
 
-            Course c1 = new Course("Java for mortals");
-            Review review4JavaForMortals = new Review();
-            review4JavaForMortals.setReviewText("Java for mortals - sucks");
-            c1.addReview(review4JavaForMortals);
-            tmp.addCourse(c1);
-            session.save(review4JavaForMortals);
-            session.save(c1);
-
-            Course c2 = new Course("Guava");
-            Lecturer lecturer2 = session.get(Lecturer.class, 3L);
-            lecturer2.addCourse(c2);
-            session.save(c2);
-
-
-
-
-
-
-
-            Lecturer managedLecturer = session.get(Lecturer.class, 3L);
-            System.out.println(managedLecturer);
+            l1.addCourse(c);
+            session.save(l1);
+//
+//
+//
+////            LecturerDetails lect1Details2 = new LecturerDetails("maxx@mail.com");
+////            Lecturer l2 = new Lecturer("Max");
+////            l2.setLecturerDetails(lect1Details2);
+////            session.save(l2);
+//
+//
+//
+//
+//
+//
+//
+//            tmp.addCourse(c);
+//            session.save(c);
+//
+//            Course c1 = new Course("Java for mortals");
+//            Review review4JavaForMortals = new Review();
+//            review4JavaForMortals.setReviewText("Java for mortals - sucks");
+//            c1.addReview(review4JavaForMortals);
+//            tmp.addCourse(c1);
+//            session.save(review4JavaForMortals);
+//            session.save(c1);
+//
+//            Course c2 = new Course("Guava");
+//            Lecturer lecturer2 = session.get(Lecturer.class, 3L);
+//            lecturer2.addCourse(c2);
+//            session.save(c2);
+//
+//
+//
+//
+//
+//
+//
+//            Lecturer managedLecturer = session.get(Lecturer.class, 3L);
+//            System.out.println(managedLecturer);
 
 
 
